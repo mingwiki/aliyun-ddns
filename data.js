8b7dabb7e70284fed918af339ae581ffb06f9a72
mingwiki <mingwiki@gmail.com>
2022-07-26 09:36:16 +0800

    优化代码

--- data.js
+++ data.js
@@ -1,9 +1,10 @@
 import Core from "@alicloud/pop-core"
 import fs from "fs"
 
+const { AccessKeyID, AccessKeySecret, testIpv4, testIpv6, Domain } = process.env
 const key = {
-  accessKeyId: process.env.Id,
-  accessKeySecret: process.env.Secret,
+  accessKeyId: AccessKeyID,
+  accessKeySecret: AccessKeySecret,
   // securityToken: '<your-sts-token>', // use STS Token
   endpoint: "https://alidns.cn-hangzhou.aliyuncs.com",
   apiVersion: "2015-01-09"
@@ -12,15 +13,13 @@ const requestOption = {
   method: "POST",
   formatParams: false
 }
-const testIpv4 = "http://4.ipw.cn"
-const testIpv6 = "http://6.ipw.cn"
 
-export const ipv4AndIpv6 = [{ url: testIpv4, Type: "A" }, { url: testIpv6, Type: "AAAA" }]
+export const ipv4AndIpv6 = [{ url: testIpv4 || "http://4.ipw.cn", Type: "A" }, { url: testIpv6 || "http://6.ipw.cn", Type: "AAAA" }]
 
-export const DomainName = "naizi.fun"
+export const DomainName = Domain
 
-export const log = data => { if (data) console.log(`${new Date().toString()}\n${data}\n`) }
+export const log = (data) => { if (data) console.log(`${new Date().toString()}\n${data}\n`) }
 
-export const write = data => { if (data) fs.appendFile("./ip.log", `${new Date().toString()}\n${data}\n`, err => log(err)) }
+export const write = (data) => { if (data) fs.appendFile("./ip.log", `${new Date().toString()}\n${data}\n`, (err) => log(err)) }
 
 export const request = (info, obj) => new Core(key).request(info, obj, requestOption)
--- index.js
+++ index.js
@@ -4,14 +4,14 @@ import { ipv4AndIpv6, DomainName, log, write, request } from "./data.js"
 const update = (RecordId, RR, Type, Value) =>
   request(
     "UpdateDomainRecord", { RecordId, RR, Type, Value })
-    .then(result => log(result), ex => log(ex))
+    .then((result) => log(result), (error) => log(error))
     .catch((error) => log(error))
 
-const sketch = (resources) => resources.forEach(item => fetch(item.url).then((res) => res.text())
-  .then(ip => {
+const sketch = (resources) => resources.forEach((item) => fetch(item.url).then((res) => res.text())
+  .then((ip) => {
     write(ip)
     request("DescribeDomainRecords", { DomainName })
-      .then(result => result.DomainRecords.Record.filter(i => i.Type === item.Type).forEach(i => update(i.RecordId, i.RR, i.Type, ip.includes(":") ? [ip] : ip)), ex => log(ex))
+      .then((result) => result.DomainRecords.Record.filter((i) => i.Type === item.Type).forEach((i) => update(i.RecordId, i.RR, i.Type, ip.includes(":") ? [ip] : ip)), (error) => log(error))
       .catch((error) => log(error))
   }).catch((error) => log(error)))
 
