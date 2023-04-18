feb067b266a93e59daec06ea4d741ef1d0e0abb0
mingwiki <mingwiki@gmail.com>
2023-04-18 19:24:00 +0800

    update

--- index.js
+++ index.js
@@ -1,36 +1,45 @@
 import {
-  ipv4AndIpv6,
   DomainNames,
-  log,
-  request,
+  ipv4s,
+  ipv6s,
+  ipv4,
+  ipv6,
   ipv4Reg,
   ipv6Reg,
+  log,
+  request,
   update,
-} from './data.js'
-
-const sketch = (items, DomainName) =>
-  request('DescribeDomainRecords', { DomainName })
-    .then((result) =>
-      items.map(async (item) => {
-        try {
-          log(item.url)
-          const ip = await fetch(item.url)
-            .then((res) => res.text())
-            .catch((err) => log(err))
-          if (ipv4Reg.test(ip) || ipv6Reg.test(ip)) {
-            update(item, result, ip)
-          } else {
-            log(item.backupUrl)
-            const ip = await fetch(item.backupUrl)
-              .then((res) => res.text())
-              .catch((err) => log(err))
-            update(item, result, ip)
-          }
-        } catch (error) {
-          log(error)
+  getIp,
+} from './utils.js'
+import { readFileSync, writeFileSync } from 'fs'
+const app = async (v, servers, reg, type, path) => {
+  const preIp = readFileSync(path, 'utf-8')
+  const name = type === 'A' ? 'ipv4' : 'ipv6'
+  if (v) {
+    for (let i = 0; i < servers.length; i++) {
+      const res = await getIp(servers[i])
+      const ip = res?.replace(/[\n\t\r]/g, '').trim()
+      log(`${servers[i]}: ${ip}`)
+      if (reg.test(ip)) {
+        if (ip === preIp) {
+          log(name + '未改动')
+        } else {
+          DomainNames.map((DomainName) => {
+            request('DescribeDomainRecords', { DomainName })
+              .then((result) => update(type, result, ip))
+              .catch(log)
+          })
         }
-      })
-    )
-    .catch((error) => log(error))
-
-DomainNames.map((DomainName) => sketch(ipv4AndIpv6, DomainName))
+        writeFileSync(path, ip, 'utf-8')
+        return
+      } else {
+        writeFileSync(path, '', 'utf-8')
+      }
+    }
+    if (!preIp) {
+      log('未找到' + name + '请检查网络')
+    }
+  }
+}
+app(ipv4, ipv4s, ipv4Reg, 'A', './pre4')
+app(ipv6, ipv6s, ipv6Reg, 'AAAA', './pre6')
--- utils.js
+++ utils.js
@@ -2,16 +2,15 @@ import Core from '@alicloud/pop-core'
 const {
   AccessKeyID,
   AccessKeySecret,
-  testIpv4,
-  testIpv6,
-  backupIpv4,
-  backupIpv6,
-  Domain,
+  Domains,
+  ipv4_servers,
+  ipv6_servers,
+  ipv4_enabled,
+  ipv6_enabled,
 } = process.env
 const key = {
   accessKeyId: AccessKeyID,
   accessKeySecret: AccessKeySecret,
-  // securityToken: "<your-sts-token>", // use STS Token
   endpoint: 'https://alidns.cn-hangzhou.aliyuncs.com',
   apiVersion: '2015-01-09',
 }
@@ -19,38 +18,61 @@ const requestOption = {
   method: 'POST',
   formatParams: false,
 }
-export const ipv4AndIpv6 = [
-  { url: testIpv4, backupUrl: backupIpv4, Type: 'A' },
-  { url: testIpv6, backupUrl: backupIpv6, Type: 'AAAA' },
+const parseEnv = (s) =>
+  s
+    .split(',')
+    .map((i) => i.trim())
+    .filter((i) => !!i)
+const [DomainNames, ipv4s, ipv6s, ipv4, ipv6] = [
+  parseEnv(Domains),
+  parseEnv(ipv4_servers),
+  parseEnv(ipv6_servers),
+  ipv4_enabled,
+  ipv6_enabled,
 ]
-export const DomainNames = Domain.split(',').map((i) => i.trim())
-export const log = (data) => {
-  if (data) {
-    console.log(`${new Date().toString()}\n${JSON.stringify(data)}\n`)
-  }
-}
-export const request = (info, obj) =>
-  new Core(key).request(info, obj, requestOption)
-export const ipv4Reg =
+
+const ipv4Reg =
   /^(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/
-export const ipv6Reg =
+const ipv6Reg =
   /^(([0-9a-fA-F]{1,4}:){7,7}[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,7}:|([0-9a-fA-F]{1,4}:){1,6}:[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,5}(:[0-9a-fA-F]{1,4}){1,2}|([0-9a-fA-F]{1,4}:){1,4}(:[0-9a-fA-F]{1,4}){1,3}|([0-9a-fA-F]{1,4}:){1,3}(:[0-9a-fA-F]{1,4}){1,4}|([0-9a-fA-F]{1,4}:){1,2}(:[0-9a-fA-F]{1,4}){1,5}|[0-9a-fA-F]{1,4}:((:[0-9a-fA-F]{1,4}){1,6})|:((:[0-9a-fA-F]{1,4}){1,7}|:)|fe80:(:[0-9a-fA-F]{0,4}){0,4}%[0-9a-zA-Z]{1,}|::(ffff(:0{1,4}){0,1}:){0,1}((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])|([0-9a-fA-F]{1,4}:){1,4}:((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9]))$/
-export const UpdateDomainRecord = (RecordId, RR, Type, Value) =>
+const log = (data) => {
+  data && console.log(`${new Date().toString()}\n${JSON.stringify(data)}\n`)
+}
+const request = (info, obj) => new Core(key).request(info, obj, requestOption)
+const UpdateDomainRecord = (RecordId, RR, Type, Value) =>
   request('UpdateDomainRecord', {
     RecordId,
     RR,
     Type,
     Value,
   })
-    .then((result) => log(result))
-    .catch((error) => log(error))
-export const update = (item, result, ip) => {
+    .then(log)
+    .catch(log)
+const update = (type, result, ip) => {
   try {
-    log(ip)
-    result.DomainRecords.Record.filter((i) => i.Type === item.Type).forEach(
-      (i) => UpdateDomainRecord(i.RecordId, i.RR, i.Type, ip)
+    log(`将更新${type === 'A' ? 'ipv4' : 'ipv6'}为: ${ip}`)
+    result.DomainRecords.Record.filter((i) => i.Type === type).forEach((i) =>
+      UpdateDomainRecord(i.RecordId, i.RR, i.Type, ip)
     )
   } catch (error) {
     log(error)
   }
+}
+const getIp = async (url) => {
+  return await fetch(url)
+    .then((res) => res.text())
+    .catch(log)
+}
+export {
+  DomainNames,
+  ipv4s,
+  ipv6s,
+  ipv4,
+  ipv6,
+  ipv4Reg,
+  ipv6Reg,
+  log,
+  request,
+  update,
+  getIp,
 }
