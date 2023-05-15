e0b4084654cf42c7fb86c864a8dea77813abcc5e
mingwiki <mingwiki@gmail.com>
2023-05-15 11:00:01 +0800

    update

--- index.js
+++ index.js
@@ -13,34 +13,36 @@ import {
   getLastLine,
   appendLastLine,
 } from './utils.js'
-const app = async (v, servers, reg, type, path) => {
+const app = async (servers, reg, type, path) => {
   const preIp = getLastLine(path)
   const name = type === 'A' ? 'ipv4' : 'ipv6'
-  if (v) {
-    for (const server of servers) {
-      const res = await getIp(server)
-      const ip = res?.replace(/[\n\t\r]/g, '').trim()
-      log(`${server}: ${ip}`)
-      if (reg.test(ip)) {
-        if (ip === preIp) {
-          log(name + '未改动')
-        } else {
-          DomainNames.map((DomainName) => {
-            request('DescribeDomainRecords', { DomainName })
-              .then((result) => update(type, result, ip))
-              .catch(log)
-          })
-          appendLastLine(path, ip)
-        }
-        return
+  for (const server of servers) {
+    const res = await getIp(server)
+    const ip = res?.replace(/[\n\t\r]/g, '').trim()
+    log(`${server}: ${ip}`)
+    if (reg.test(ip)) {
+      if (ip === preIp) {
+        log(name + '未改动')
       } else {
-        appendLastLine(path, 'unknown')
+        DomainNames.map((DomainName) => {
+          request('DescribeDomainRecords', { DomainName })
+            .then((result) => update(type, result, ip))
+            .catch(log)
+        })
+        appendLastLine(path, ip)
       }
-    }
-    if (!preIp || preIp === 'unknown') {
-      log('未找到' + name + '请检查网络')
+      return
+    } else {
+      appendLastLine(path, 'unknown')
     }
   }
+  if (!preIp || preIp === 'unknown') {
+    log('未找到' + name + '请检查网络')
+  }
 }
-app(ipv4, ipv4s, ipv4Reg, 'A', './pre4')
-app(ipv6, ipv6s, ipv6Reg, 'AAAA', './pre6')
+if (ipv4 === 'true') {
+  app(ipv4s, ipv4Reg, 'A', './pre4')
+}
+if (ipv6 === 'true') {
+  app(ipv6s, ipv6Reg, 'AAAA', './pre6')
+}
