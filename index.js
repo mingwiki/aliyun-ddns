a864d0fbcd3015abd273e1aa0d1403bf3527b751
mingwiki <mingwiki@gmail.com>
2023-06-06 11:02:31 +0800

    update

--- index.js
+++ index.js
@@ -1,48 +1,39 @@
 import {
-  DomainNames,
-  ipv4s,
-  ipv6s,
-  ipv4,
-  ipv6,
-  ipv4Reg,
-  ipv6Reg,
-  log,
-  request,
-  update,
-  getIp,
-  getLastLine,
-  appendLastLine,
+    DomainNames, ipv4s, ipv6s, enabled, ipv4Reg, ipv6Reg, log, request, update, getIp, getLastLine, appendLastLine,
 } from "./utils.js";
+
 const app = async (servers, reg, type, path) => {
-  const preIp = getLastLine(path);
-  const name = type === "A" ? "ipv4" : "ipv6";
-  for (const server of servers) {
-    const res = await getIp(server);
-    const ip = res?.replace(/[\n\t\r]/g, "").trim();
-    log(`${server}: ${ip}`);
-    if (reg.test(ip)) {
-      if (ip === preIp) {
-        log(name + "未改动");
-      } else {
-        DomainNames.map((DomainName) => {
-          request("DescribeDomainRecords", { DomainName })
-            .then((result) => update(type, result, ip))
-            .catch(log);
-        });
-        appendLastLine(path, ip);
-      }
-      return;
-    } else {
-      if (reg.test(preIp)) {
-        appendLastLine(path, "unknown");
-      }
-      log("未找到" + name + "请检查网络");
+    const preIp = getLastLine(path);
+    const name = type === "A" ? "ipv4" : "ipv6";
+    for (const server of servers) {
+        const res = await getIp(server);
+        const ip = res?.replace(/[\n\t\r]/g, "").trim();
+        log(`${server}: ${ip}`);
+        if (reg.test(ip)) {
+            if (ip === preIp) {
+                log(name + "未改动");
+            } else {
+                DomainNames.map((DomainName) => {
+                    request("DescribeDomainRecords", {DomainName})
+                        .then((result) => update(type, result, ip))
+                        .catch(log);
+                });
+                appendLastLine(path, ip);
+            }
+            return;
+        } else {
+            if (reg.test(preIp)) {
+                appendLastLine(path, "unknown");
+            }
+            log("未找到" + name + "请检查网络");
+        }
     }
-  }
 };
-if (ipv4 === "true") {
-  app(ipv4s, ipv4Reg, "A", "./ipv4.log");
-}
-if (ipv6 === "true") {
-  app(ipv6s, ipv6Reg, "AAAA", "./ipv6.log");
-}
+enabled.map(i => {
+    if (i === 'v4') {
+        app(ipv4s, ipv4Reg, "A", "./ipv4.log");
+    }
+    if (i === 'v6') {
+        app(ipv6s, ipv6Reg, "AAAA", "./ipv6.log");
+    }
+})
--- utils.js
+++ utils.js
@@ -1,92 +1,63 @@
 import Core from '@alicloud/pop-core'
-import { existsSync, readFileSync, appendFile } from 'fs'
+import {existsSync, readFileSync, appendFile} from 'fs'
+
 const {
-  AccessKeyID,
-  AccessKeySecret,
-  Domains,
-  ipv4_servers,
-  ipv6_servers,
-  ipv4_enabled,
-  ipv6_enabled,
+    AccessKeyID, AccessKeySecret, Domains, ipv4_servers, ipv6_servers, ip_enabled
 } = process.env
 const key = {
-  accessKeyId: AccessKeyID,
-  accessKeySecret: AccessKeySecret,
-  endpoint: 'https://alidns.cn-hangzhou.aliyuncs.com',
-  apiVersion: '2015-01-09',
+    accessKeyId: AccessKeyID,
+    accessKeySecret: AccessKeySecret,
+    endpoint: 'https://alidns.cn-hangzhou.aliyuncs.com',
+    apiVersion: '2015-01-09',
 }
 const requestOption = {
-  method: 'POST',
-  formatParams: false,
+    method: 'POST', formatParams: false,
 }
-const parseEnv = (s) =>
-  s
+const parseEnv = (s) => s
     .split(',')
     .map((i) => i.trim())
     .filter((i) => !!i)
-const [DomainNames, ipv4s, ipv6s, ipv4, ipv6] = [
-  parseEnv(Domains),
-  parseEnv(ipv4_servers),
-  parseEnv(ipv6_servers),
-  ipv4_enabled,
-  ipv6_enabled,
-]
+const [DomainNames, ipv4s, ipv6s, enabled] = [parseEnv(Domains), parseEnv(ipv4_servers), parseEnv(ipv6_servers), parseEnv(ip_enabled)]
 
-const ipv4Reg =
-  /^(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/
-const ipv6Reg =
-  /^(([0-9a-fA-F]{1,4}:){7,7}[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,7}:|([0-9a-fA-F]{1,4}:){1,6}:[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,5}(:[0-9a-fA-F]{1,4}){1,2}|([0-9a-fA-F]{1,4}:){1,4}(:[0-9a-fA-F]{1,4}){1,3}|([0-9a-fA-F]{1,4}:){1,3}(:[0-9a-fA-F]{1,4}){1,4}|([0-9a-fA-F]{1,4}:){1,2}(:[0-9a-fA-F]{1,4}){1,5}|[0-9a-fA-F]{1,4}:((:[0-9a-fA-F]{1,4}){1,6})|:((:[0-9a-fA-F]{1,4}){1,7}|:)|fe80:(:[0-9a-fA-F]{0,4}){0,4}%[0-9a-zA-Z]{1,}|::(ffff(:0{1,4}){0,1}:){0,1}((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])|([0-9a-fA-F]{1,4}:){1,4}:((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9]))$/
+const ipv4Reg = /^(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/
+const ipv6Reg = /^(([0-9a-fA-F]{1,4}:){7,7}[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,7}:|([0-9a-fA-F]{1,4}:){1,6}:[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,5}(:[0-9a-fA-F]{1,4}){1,2}|([0-9a-fA-F]{1,4}:){1,4}(:[0-9a-fA-F]{1,4}){1,3}|([0-9a-fA-F]{1,4}:){1,3}(:[0-9a-fA-F]{1,4}){1,4}|([0-9a-fA-F]{1,4}:){1,2}(:[0-9a-fA-F]{1,4}){1,5}|[0-9a-fA-F]{1,4}:((:[0-9a-fA-F]{1,4}){1,6})|:((:[0-9a-fA-F]{1,4}){1,7}|:)|fe80:(:[0-9a-fA-F]{0,4}){0,4}%[0-9a-zA-Z]{1,}|::(ffff(:0{1,4}){0,1}:){0,1}((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])|([0-9a-fA-F]{1,4}:){1,4}:((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9]))$/
 const log = (data) => {
-  data && console.log(`${new Date().toString()}\n${JSON.stringify(data)}\n`)
+    data && console.log(`${new Date().toString()}\n${JSON.stringify(data)}\n`)
 }
 const request = (info, obj) => new Core(key).request(info, obj, requestOption)
-const UpdateDomainRecord = (RecordId, RR, Type, Value) =>
-  request('UpdateDomainRecord', {
-    RecordId,
-    RR,
-    Type,
-    Value,
-  })
+const UpdateDomainRecord = (RecordId, RR, Type, Value) => request('UpdateDomainRecord', {
+    RecordId, RR, Type, Value,
+})
     .then(log)
     .catch(log)
 const update = (type, result, ip) => {
-  try {
-    log(`将更新${type === 'A' ? 'ipv4' : 'ipv6'}为: ${ip}`)
-    result.DomainRecords.Record.filter((i) => i.Type === type).forEach((i) => {
-      log(i)
-      UpdateDomainRecord(i.RecordId, i.RR, i.Type, ip)
-    })
-  } catch (error) {
-    log(error)
-  }
+    try {
+        log(`将更新${type === 'A' ? 'ipv4' : 'ipv6'}为: ${ip}`)
+        result.DomainRecords.Record.filter((i) => i.Type === type).forEach((i) => {
+            log(i)
+            UpdateDomainRecord(i.RecordId, i.RR, i.Type, ip)
+        })
+    } catch (error) {
+        log(error)
+    }
 }
 const getIp = async (url) => {
-  return await fetch(url)
-    .then((res) => res.text())
-    .catch(log)
+    return await fetch(url)
+        .then((res) => res.text())
+        .catch(log)
 }
 export const getLastLine = (path) => {
-  if (existsSync(path)) {
-    const data = readFileSync(path, 'utf8')
-    const lines = data.split('\n')
-    return lines[lines.length - 1]
-  } else {
-    return 'unknown'
-  }
+    if (existsSync(path)) {
+        const data = readFileSync(path, 'utf8')
+        const lines = data.split('\n')
+        return lines[lines.length - 1]
+    } else {
+        return 'unknown'
+    }
 }
 export const appendLastLine = (path, data) => {
-  appendFile(path, `\n${new Date().toString()}\n${data}`, log)
+    appendFile(path, `\n${new Date().toString()}\n${data}`, log)
 }
 export {
-  DomainNames,
-  ipv4s,
-  ipv6s,
-  ipv4,
-  ipv6,
-  ipv4Reg,
-  ipv6Reg,
-  log,
-  request,
-  update,
-  getIp,
+    DomainNames, ipv4s, ipv6s, enabled, ipv4Reg, ipv6Reg, log, request, update, getIp,
 }
